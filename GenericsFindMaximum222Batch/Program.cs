fa415fb8170ffe7a6e653135130245686c8b96db
AnilBamne <anilbamne20@gmail.com>
2022-11-28 08:46:13 +0530

    Adding UC1FindMaximumOfThreeIntegers code

--- GenericsFindMaximum222Batch/Program.cs
+++ GenericsFindMaximum222Batch/Program.cs
@@ -7,6 +7,24 @@ namespace GenericsFindMaximum222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics find maximum program");
+            Console.WriteLine("Enter the use case to execute\n1:UC1 find max of three integers\n2:UC2 find max of three floats\n3:UC3 find max of three strings\n4:Refactor all three in one generic method\n");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1 :
+                    ThreeIntegers.FindMaxValue();
+                    break;
+                case 2 :
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Invalid option , Please provide a valid input");
+                    break;
+            }
+
         }
     }
 }
---
+++ GenericsFindMaximum222Batch/ThreeIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMaximum222Batch
+{
+    internal class ThreeIntegers
+    {
+        //Taking input of three integers and finding maximum of them.
+        //using CompareTo method.
+        public static void FindMaxValue()       
+        {
+            Console.WriteLine("Enter three Integer values");
+            int firstNum=int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                Console.WriteLine("{0} is greater", firstNum);
+            }
+            else if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+            {
+                Console.WriteLine("{0} is greater", secondNum);
+            }
+            else if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                Console.WriteLine("{0} is greater", thirdNum);
+            }
+            else
+                Console.WriteLine("All the mumbers are Equal ");
+        }
+    }
+}
