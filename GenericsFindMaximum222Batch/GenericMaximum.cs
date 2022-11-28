baf6c8d2a0ec2a9066b438b5e56cda383a348ce2
AnilBamne <anilbamne20@gmail.com>
2022-11-28 16:57:11 +0530

    Adding UC4ExtendMaxMethodToGenericMethod code

---
+++ GenericsFindMaximum222Batch/GenericMaximum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMaximum222Batch
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T FindMaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            else
+                return default;
+        }
+        public T MaxMethod()
+        {
+            T max = FindMaxValue(firstValue,secondValue,thirdValue);
+            Console.WriteLine(max);
+            return max;
+        }
+    }
+}
--- GenericsFindMaximum222Batch/Program.cs
+++ GenericsFindMaximum222Batch/Program.cs
@@ -7,7 +7,8 @@ namespace GenericsFindMaximum222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics find maximum program");
-            Console.WriteLine("Enter the use case to execute\n1:UC1 find max of three integers\n2:UC2 find max of three floats\n3:UC3 find max of three strings\n4:Refactor all three in one generic method\n5:Refactor2 Geneeric class");
+            Console.WriteLine("Enter the use case to execute\n1:UC1 find max of three integers\n2:UC2 find max of three floats\n3:UC3 find max of three strings\n4:Refactor all three in one generic method\n5:Refactor2 Geneeric class" +
+                "\n6:Extend MaxMethod to Generic methos");
             int option=int.Parse(Console.ReadLine());
             switch (option)
             {
@@ -29,6 +30,14 @@ namespace GenericsFindMaximum222Batch
                     GenricClass<int>.FindMaxValue(20,20,22);
                     GenricClass<double>.FindMaxValue(21.5,10.3,21.51);
                     GenricClass<string>.FindMaxValue("Bidar","Gulbarga","Bengaluru");
+                    break;
+                case 6:
+                    GenericMaximum<int> max = new GenericMaximum<int>(20,30,40);
+                    GenericMaximum<double> max1 = new GenericMaximum<double>(20.5,32.02,40.2);
+                    GenericMaximum<string> max2 = new GenericMaximum<string>("Bidar", "Gulbarga", "Bengaluru");
+                    max.MaxMethod();
+                    max1.MaxMethod();
+                    max2.MaxMethod();
                     break;
                 default:
                     Console.WriteLine("Invalid option , Please provide a valid input");
