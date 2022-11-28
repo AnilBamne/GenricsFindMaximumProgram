23ccd8e713a427eba6cf8215a32a1da8de7a3f47
AnilBamne <anilbamne20@gmail.com>
2022-11-28 08:19:10 +0530

    Adding Welcome Message

---
+++ GenericsFindMaximum222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsFindMaximum222Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generics find maximum program");
+        }
+    }
+}
