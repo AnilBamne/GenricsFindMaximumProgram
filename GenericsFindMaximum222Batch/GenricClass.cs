7b04ed79a35ad291375d69478115069b647711b3
AnilBamne <anilbamne20@gmail.com>
2022-11-28 15:03:00 +0530

    Adding UC3Refactor2GenericClass code

---
+++ GenericsFindMaximum222Batch/GenricClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMaximum222Batch
+{
+    internal class GenricClass<T> where T : IComparable<T>
+    {
+        //refactoring all three UCs in one generic class
+        public static void FindMaxValue(T first, T second, T third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is greater", first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is greater", second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("{0} is greater", third);
+            }
+            else
+                Console.WriteLine("All are equal. ");
+        }
+    }
+}
--- GenericsFindMaximum222Batch/Program.cs
+++ GenericsFindMaximum222Batch/Program.cs
@@ -7,7 +7,7 @@ namespace GenericsFindMaximum222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics find maximum program");
-            Console.WriteLine("Enter the use case to execute\n1:UC1 find max of three integers\n2:UC2 find max of three floats\n3:UC3 find max of three strings\n4:Refactor all three in one generic method\n");
+            Console.WriteLine("Enter the use case to execute\n1:UC1 find max of three integers\n2:UC2 find max of three floats\n3:UC3 find max of three strings\n4:Refactor all three in one generic method\n5:Refactor2 Geneeric class");
             int option=int.Parse(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,11 @@ namespace GenericsFindMaximum222Batch
                     GenericsFindMaximum.FindMaxValue<int>(12,43,11);
                     GenericsFindMaximum.FindMaxValue<double>(10.3,4.3,33.1);
                     GenericsFindMaximum.FindMaxValue<String>("John","Roman","Goldberg");
+                    break;
+                case 5:
+                    GenricClass<int>.FindMaxValue(20,20,22);
+                    GenricClass<double>.FindMaxValue(21.5,10.3,21.51);
+                    GenricClass<string>.FindMaxValue("Bidar","Gulbarga","Bengaluru");
                     break;
                 default:
                     Console.WriteLine("Invalid option , Please provide a valid input");
